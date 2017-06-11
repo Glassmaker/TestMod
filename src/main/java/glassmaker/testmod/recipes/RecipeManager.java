aa7bf0fd66bda9dd35a2b78e2cfe065489154635
Glassmaker <JeanGlassmaker@mail.com>
2017-06-11 23:41:29 +0300

    Added Shaped recipe exmaple

--- src/main/java/glassmaker/testmod/recipes/RecipeManager.java
+++ src/main/java/glassmaker/testmod/recipes/RecipeManager.java
@@ -1,5 +1,12 @@
 package glassmaker.testmod.recipes;
 
+import java.util.Map;
+import java.util.Set;
+
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+import com.google.gson.JsonSyntaxException;
+
 import glassmaker.testmod.TestMod;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
@@ -7,6 +14,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.CraftingManager;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.crafting.ShapedRecipes;
 import net.minecraft.item.crafting.ShapelessRecipes;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
@@ -15,11 +23,48 @@ public class RecipeManager {
 
 	public static void register(){
 		
+		String[] pattern = {"ccc", "cac", "ccc"};
+		Map<String,Ingredient>keyMap = Maps.newHashMap();
+		keyMap.put("c", Ingredient.func_193369_a(new ItemStack(Items.COAL)));
+		keyMap.put("a", Ingredient.func_193369_a(new ItemStack(Items.APPLE)));
+		
+		addShapedRecipe(new ResourceLocation("testmod:test_shaped_recipe"),"",pattern,keyMap, new ItemStack(TestMod.block));
+		
 		addShapelessRecipe(new ResourceLocation("testmod:test_shapeless_recipe"), "", new ItemStack(TestMod.block), new ItemStack(Items.APPLE), new ItemStack(Items.STICK));
 	}
 	
-	private static void addShapedRecipe(){
-		
+	private static void addShapedRecipe(ResourceLocation Identifier, String group, String[] pattern, Map<String, Ingredient> ingredientKeyMap, ItemStack output){
+		int width = pattern[0].length();
+		int height = pattern.length;
+		NonNullList<Ingredient> IngredientPattern = buildIngredientPattern(pattern, ingredientKeyMap, width, height);
+		IRecipe shapedRecipe = new ShapedRecipes(group, width, height, IngredientPattern, output);
+		CraftingManager.func_193372_a(Identifier, shapedRecipe);
+	}
+	
+	private static NonNullList<Ingredient> buildIngredientPattern(String[] pattern, Map<String, Ingredient> ingredientKeyMap, int patternWidth, int patternHeight) {
+		NonNullList<Ingredient> ingredientPattern = NonNullList.<Ingredient>withSize(patternWidth * patternHeight, Ingredient.field_193370_a);
+        Set<String> set = Sets.newHashSet(ingredientKeyMap.keySet());
+        set.remove(" ");
+
+        for (int i = 0; i < pattern.length; i++) {
+            for (int j = 0; j < pattern[i].length(); j++) {
+                String patternSymbol = pattern[i].substring(j, j + 1);
+                Ingredient ingredient = ingredientKeyMap.get(patternSymbol);
+
+                if (ingredient == null){
+                    throw new RuntimeException("Undefined pattern symbol '" + patternSymbol + "' ");
+                }
+
+                set.remove(patternSymbol);
+                ingredientPattern.set(j + patternWidth * i, ingredient);
+            }
+        }
+
+        if (!set.isEmpty()) {
+            throw new RuntimeException("Unused Keys: " + set);
+        } else {
+            return ingredientPattern;
+        }
 	}
 	
 	private static void addShapelessRecipe(ResourceLocation identifier, String group, ItemStack output, ItemStack ... ingredients ) {
